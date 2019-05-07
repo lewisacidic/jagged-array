9f1bdebb3d375619bae79b857bc877c44d2dfcb6
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-08 00:21:48 +0100

    feat(slicing): scaffold slicing support

---
+++ jagged/slicing.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Rich Lewis <opensource@richlew.is>
+# License: MIT
+"""
+jagged.slicing
+~~~~~~~~~~~~~~
+
+Slicing support for jagged-array.
+"""
+
+
+def normalize_index(idx, shape):
+    """ Convert an index into its canonical form for a given shape array. """
+
+    pass
---
+++ jagged/tests/test_slicing.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Rich Lewis <opensource@richlew.is>
+# License: MIT
+""" Tests for slicing of jagged arrays. """
