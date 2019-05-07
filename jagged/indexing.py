b5680c3ddc8746fe814859ae5c7e597d4089d247
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-08 00:22:26 +0100

    feat(indexing): scaffold indexing support

---
+++ jagged/indexing.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Rich Lewis <opensource@richlew.is>
+# License: MIT
+"""
+jagged.indexing
+~~~~~~~~~~~~~~~
+
+Indexing support for jagged arrays.
+"""
+
+
+def getitem(x, index):
+    """ Index a JaggedArray. """
+
+    pass
---
+++ jagged/tests/test_indexing.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Rich Lewis <opensource@richlew.is>
+# License: MIT
+""" Tests for indexing of jagged arrays. """
