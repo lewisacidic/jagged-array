650520acda649c3357981308b982c13a6ff6eefc
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-10 15:14:21 +0100

    feat(utils): add shapes conversion utils
    Add shapes_to_shape to calculate a shape from shapes, and shapes_to_size
    to calculate a shape from a size.

--- jagged/utils.py
+++ jagged/utils.py
@@ -14,6 +14,7 @@ from typing import Any
 import numpy as np
 
 from .typing import DtypeLike
+from .typing import JaggedShapeLike
 
 
 def infer_nan(dtype: DtypeLike) -> Any:
@@ -73,3 +74,51 @@ def shape_to_shapes(shape):
     for i, ax in enumerate(axs):
         res[:, i] = ax
     return res
+
+
+def shapes_to_shape(shapes: np.ndarray) -> JaggedShapeLike:
+    """ Convert an array of shapes to a jagged shape.
+
+    Args:
+        shapes:
+            An array of shapes
+
+    Examples:
+        >>> shapes_to_shape([[1], [3], [2]])
+        (3, (1, 3, 2))
+
+        >>> shapes_to_shape([[1, 3], [3, 2], [2, 3]])
+        (3, (1, 3, 2), (3, 2, 3))
+
+        >>> shapes_to_shape([[3, 2], [1, 2], [1, 2]])
+        (3, (3, 1, 1), 2)
+
+    See Also:
+        shape_to_shapes: the reverse of this function
+    """
+    shapes = np.asarray(shapes)
+    rest = (dim[0] if (dim == dim[0]).all() else tuple(dim) for dim in shapes.T)
+    return (len(shapes), *rest)
+
+
+def shapes_to_size(shapes: JaggedShapeLike) -> int:
+    """ Get the number of entries for an array of shapes.
+
+    Args:
+        shapes:
+            An array of shapes
+
+    Examples:
+        >>> size_from_shape((2, (1, 2)))
+        3
+
+        >>> size_from_shape((3, (2, 3, 2), 2))
+        14
+
+        >>> size_from_shape((3, (2, 3, 2), (1, 4, 2)))
+        18
+
+        >>> size_from_shape((3, 2, (2, 3, 2), (1, 4, 2)))
+        36
+    """
+    return shapes.prod(axis=1).sum()
