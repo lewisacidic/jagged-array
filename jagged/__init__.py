236a48a6019cd37d2223bb5596e3838c9a7df3d3
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-10 16:45:22 +0100

    feat(base): import api into base of package

--- jagged/__init__.py
+++ jagged/__init__.py
@@ -9,7 +9,36 @@ jagged
 
 Jagged array support for the pydata ecosystem.
 """
+from .api import concatenate
+from .api import diagonal
+from .api import expand_dims
+from .api import full
+from .api import full_like
+from .api import ones
+from .api import ones_like
+from .api import random
+from .api import squeeze
+from .api import stack
+from .api import trace
+from .api import where
+from .api import zeros
+from .api import zeros_like
 from .core import JaggedArray
-from .utils import random
 
-__all__ = ["JaggedArray", "random"]
+__all__ = [
+    "JaggedArray",
+    "random",
+    "concatenate",
+    "squeeze",
+    "expand_dims",
+    "zeros",
+    "zeros_like",
+    "ones",
+    "ones_like",
+    "full",
+    "full_like",
+    "stack",
+    "trace",
+    "diagonal",
+    "where",
+]
