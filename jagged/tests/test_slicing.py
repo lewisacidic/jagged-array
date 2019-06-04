a5c65bd826cf00230c054c9b9e044770dfa72f0c
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-06-04 17:28:10 +0100

    test(slicing): add one d slicing tests

--- jagged/tests/test_slicing.py
+++ jagged/tests/test_slicing.py
@@ -43,3 +43,39 @@ from .utils import raises
 def test_zero_d(object, index, desired, expectation):
     with expectation:
         assert_flat_equal(canonicalize_index(object(index), (10,)), desired)
+
+
+@pytest.mark.parametrize(
+    "object",
+    [
+        param(lambda x: x, id="list"),
+        param(lambda x: (tuple(x),), id="tuple"),
+        param(lambda x: np.array(x), id="array"),
+    ],
+)
+@pytest.mark.parametrize(
+    ["index", "desired", "expectation"],
+    [
+        param([1, 3], (np.array([1, 3]),), does_not_raise(), id="standard"),
+        param([3, 1], (np.array([3, 1]),), does_not_raise(), id="out of order"),
+        param(
+            [3, 7, 1], (np.array([3, 7, 1]),), does_not_raise(), id="more out of order"
+        ),
+        param([0, 1, 3], (np.array([0, 1, 3]),), does_not_raise(), id="low"),
+        param([6, 8, 9], (np.array([6, 8, 9]),), does_not_raise(), id="high"),
+        param([0, 9], (np.array([0, 9]),), does_not_raise(), id="edges"),
+        param([9, 10], None, raises(IndexError), id="just too high"),
+        param([5, 30], None, raises(IndexError), id="too high"),
+        param([-7, -3], (np.array([3, 7]),), does_not_raise(), id="negative"),
+        param([3, -1], (np.array([3, 9]),), does_not_raise(), id="negative low"),
+        param([3, -10], (np.array([3, 0]),), does_not_raise(), id="negative high"),
+        param([-10, -1], (np.array([0, 9]),), does_not_raise(), id="negative edges"),
+        param([-11, -15], None, raises(IndexError), id="negative too high"),
+        param([-20, -15], None, raises(IndexError), id="negative too high"),
+        param([0.5, 1.5], None, raises(IndexError), id="float"),
+        param(["T", "S"], None, raises(IndexError), id="string"),
+    ],
+)
+def test_one_d(object, index, desired, expectation):
+    with expectation:
+        assert_flat_equal(canonicalize_index(object(index), (10,)), desired)
